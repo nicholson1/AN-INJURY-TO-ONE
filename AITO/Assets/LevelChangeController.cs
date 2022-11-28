71da4f1aafab08d5618ab74003650ea638963f33
Jiachen Tang <tjc9606ok@gmail.com>
2022-11-27 22:12:40 -0500

    add Observer Pattern for Scene End Triggers

--- AITO/Assets/LevelChangeController.cs
+++ AITO/Assets/LevelChangeController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LevelChangeController : MonoBehaviour
+public class LevelChangeController : MonoBehaviour, ObserverOfSceneEndTrigger
 {
     private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Image>().enabled = true;
-        animator = GetComponent<Animator>();
+        //gameObject.GetComponent<Image>().enabled = true;
+        //animator = GetComponent<Animator>();
+        PrepareOnLoad();
     }
 
     // Update is called once per frame
@@ -21,10 +22,41 @@ public class LevelChangeController : MonoBehaviour
         
     }
 
+    protected void PrepareOnLoad()
+    {
+        gameObject.GetComponent<Image>().enabled = true;
+        this.animator = GetComponent<Animator>();
+
+        foreach (SceneEndTriggerController controller in FindObjectsOfType<SceneEndTriggerController>())
+        {
+            controller.AddObserverOfSceneEndTrigger(this);
+        }
+    }
+
+    public void OnNotify(int sceneIndex, bool sceneEnds)
+    {
+        if (sceneEnds)
+        {
+            this.animator.SetTrigger("FadeOut");
+            OnFadeComplete(sceneIndex);
+            Debug.Log("Scene " + sceneIndex + " Ends");
+        }
+    }
+
     public void OnFadeComplete()
     {
         // for playtest, will return to menu scene
         // need to replaced to "+ 1" after having following scenes
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void OnFadeComplete(int sceneIndex)
+    {
+        // for playtest, will return to menu scene
+        // need to replaced to "+ 1" after having following scenes
+
+        SceneManager.LoadScene(sceneIndex - 1);
     }
 }
--- AITO/Assets/SceneEndTriggerController.cs
+++ AITO/Assets/SceneEndTriggerController.cs
@@ -2,8 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneEndTriggerController : MonoBehaviour
+public abstract class SceneEndTriggerController : MonoBehaviour
 {
+    protected int sceneIndex;
+    protected bool sceneEnds = false;
+
+    private List<ObserverOfSceneEndTrigger> _observersOfSceneEndTrigger = new List<ObserverOfSceneEndTrigger>();
+
+    public abstract void ReturnLog();
+    public abstract int ReturnSceneIndex();
+
+    public void AddObserverOfSceneEndTrigger(ObserverOfSceneEndTrigger observer)
+    {
+        _observersOfSceneEndTrigger.Add(observer);
+    }
+
+    public void RemoveObserverOfSceneEndTrigger(ObserverOfSceneEndTrigger observer)
+    {
+        _observersOfSceneEndTrigger.Remove(observer);
+    }
+
+    public void Notify(int sceneIndex, bool sceneEnds)
+    {
+        foreach (ObserverOfSceneEndTrigger observer in _observersOfSceneEndTrigger)
+        {
+            observer.OnNotify(sceneIndex, sceneEnds);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
