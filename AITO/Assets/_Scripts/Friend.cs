bb7bc0a97641f13ed4cf73070809a827304baa06
lnlupa <93613563+lnlupa@users.noreply.github.com>
2022-12-02 16:30:11 -0500

    continued hazard war

--- AITO/Assets/_Scripts/Friend.cs
+++ AITO/Assets/_Scripts/Friend.cs
@@ -17,11 +17,7 @@ public class Friend : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField] private LayerMask interactLayers;
-    
-    
 
-    //remembering the default speed of the friend for hazard effects
-    private float defSpeed;
     private void Start()
     {
         PlayerFriendControl.CollectFriend += CollectedTrigger;
@@ -32,20 +28,12 @@ public class Friend : MonoBehaviour
         
         animator.SetBool("Grounded", true);
         animator.SetFloat("IdleSpeed", 1);
-
-        //subscribe to hazards
-        Hazard.FrHazardHit += HazardReact;
-        //hang onto the default speed of the friends
-        defSpeed = moveSpeed;
     }
 
     private void OnDestroy()
     {
         PlayerFriendControl.CollectFriend -= CollectedTrigger;
         PlayerFriendControl.ThrowFriend -= Throw;
-
-        //unsubscribe from hazards
-        Hazard.FrHazardHit -= HazardReact;
     }
 
     private void FixedUpdate()
@@ -171,62 +159,4 @@ public class Friend : MonoBehaviour
       
 
     }
-
-
-    //friend hazard behaviors
-    private float speedBoost = 50f;
-    private void HazardReact(Hazard.HazardType Haz, Transform respawn, GameObject FriendObj)
-    {
-        if (!isFollowing) 
-        {
-            if (FriendObj == this.gameObject) 
-            {
-                switch (Haz)
-                {
-                    case Hazard.HazardType.Lava:
-                        Debug.Log("friend lava");
-                        StartCoroutine(LavaRespawn(respawn));
-                        break;
-                    case Hazard.HazardType.Oil:
-                        Debug.Log("friend oil");
-                        StartCoroutine(OilEffect());
-                        break;
-                    case Hazard.HazardType.Electro:
-                        Debug.Log("friend zap");
-                        StartCoroutine(ElectroEffect());
-                        break;
-                }
-            }
-
-        }
-    }
-
-    //oil is very hard to test without any AI or movement on the friends
-    private IEnumerator OilEffect()
-    {
-        moveSpeed =+ speedBoost;
-        Debug.Log("friend speed " + moveSpeed);
-        yield return new WaitForSeconds(5f);
-        moveSpeed = defSpeed;
-        Debug.Log("friend speed " + moveSpeed);
-    }
-
-    //this seems to work? But is hard to test without friend movement
-    private IEnumerator ElectroEffect()
-    {
-        rb.AddForce(new Vector2(-30, 0), ForceMode2D.Impulse);
-        Debug.Log("friend velocity " + rb.velocity);
-        yield return new WaitForSeconds(2f);
-        rb.velocity = new Vector2(0,0);
-        Debug.Log("friend velocity " + rb.velocity);
-    }
-
-    //this works
-    private IEnumerator LavaRespawn(Transform rPoint)
-    {
-        Debug.Log(rPoint.position);
-        transform.position = Vector2.Lerp(transform.position, rPoint.position, 30f * Time.deltaTime);
-        yield return new WaitForSeconds(5f);
-    }
-
 }
--- AITO/Assets/_Scripts/Hazard.cs
+++ AITO/Assets/_Scripts/Hazard.cs
@@ -5,22 +5,17 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    //for now hazards only affect the player because the friends have no colliders when they are following
-
-    //public static event Action<HazardType, Transform> PlHazardHit;
-
-    public static event Action<HazardType, Transform, GameObject> FrHazardHit;
-
     public static event Action Respawn;
 
-    public Rigidbody2D Player;
+    public GameObject Player;
 
     //Hazard effects
-    /*private Rigidbody2D rb;
-    private float defAccel;
-    private float accelBump = 15f;
-    private float defDeccel;
-    private float decelBump = -15f;*/
+    private Rigidbody2D rb;
+    private Rigidbody2D frb;
+    //private float defAccel;
+    //private float accelBump = 15f;
+    //private float defDeccel;
+    //private float decelBump = -15f;
 
     public enum HazardType 
     {
@@ -31,33 +26,14 @@ public class Hazard : MonoBehaviour
 
     private void Start()
     {
+        Player = GameObject.FindGameObjectWithTag("Player");
         //getting the rigidbody so we can add force
-        //rb = Player.GetComponent<Rigidbody2D>();
+        rb = Player.GetComponent<Rigidbody2D>();
 
         //saving the player's default acceleration so we can get back to it after the oil slick
         //defAccel = _acceleration;
         //defDeccel = _deAcceleration;
     }
-
-    public HazardType ThisHazard;
-
-    //I haven't yet begun restructuring the friends' reactions to hazards, still figuring that out
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player")) 
-        {
-            //string tag = gameObject.tag;
-            //PlHazardHit?.Invoke(ThisHazard, RespawnPoints[CurrentRespawn].transform);
-            //Debug.Log(RespawnPoints[CurrentRespawn].transform.position);
-            HazardReact(ThisHazard);
-        }
-
-        if (collision.gameObject.CompareTag("Friend"))
-        {
-            //FrHazardHit?.Invoke(ThisHazard, RespawnPoints[CurrentRespawn].transform, collision.gameObject);
-        }
-    }
-
 
     private void OnDestroy()
     {
@@ -65,8 +41,39 @@ public class Hazard : MonoBehaviour
         //Hazard.PlHazardHit -= HazardReact;
     }
 
-    //function that sees what hazard was hit and what should happen because of it
-    //these all now work, but they need some tuning
+    public HazardType ThisHazard;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            HazardReact(ThisHazard);
+        }
+
+        if (collision.gameObject.CompareTag("Friend"))
+        {
+            frb = collision.gameObject.GetComponent<Rigidbody2D>();
+            FriendHazardReact(ThisHazard);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (ThisHazard == HazardType.Electro) 
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                //rb.velocity *= -2f;
+                rb.AddForce(rb.transform.right * -2f, ForceMode2D.Impulse);
+            }
+
+            if (collision.gameObject.CompareTag("Friend"))
+            {
+                //frb.velocity *= -2f;
+                frb.AddForce(frb.transform.right * -2f, ForceMode2D.Impulse);
+            }
+        }
+    }
+
     private void HazardReact(HazardType Haz)
     {
         switch (Haz)
@@ -78,32 +85,73 @@ public class Hazard : MonoBehaviour
                 break;
             case HazardType.Oil:
                 Debug.Log("player oil");
-                StartCoroutine(OilEffect());
+                //StartCoroutine(OilEffect());
                 break;
             case HazardType.Electro:
                 Debug.Log("player zap");
                 //StartCoroutine(ElectroEffect());
-                ElectroEffect();
+                //ElectroEffect();
                 break;
         }
     }
 
-    //when the player hits the oil slick, their acceleration goes up and deceleration goes down
-    //and then after a couple non-oily seconds it goes back down and decel goes back up
-    private IEnumerator OilEffect()
+    //oil currently turned off
+    /*private IEnumerator OilEffect()
     {
         //_acceleration += accelBump;
         //_deAcceleration += decelBump;
         yield return new WaitForSeconds(5f);
         //_acceleration = defAccel;
         //_deAcceleration = defDeccel;
-    }
+    }*/
 
-    private void ElectroEffect() 
+    /*private void ElectroEffect() 
     {
         //multiplying the velocity by -2 hasn't been working??
         //will continue experimenting
-        Player.AddForce(Player.transform.right * -2f, ForceMode2D.Impulse);
-        Debug.Log("this happened " + Player.velocity);
+        rb.AddForce(rb.transform.right * -2f, ForceMode2D.Impulse);
+        Debug.Log("this happened " + rb.velocity);
+    }*/
+
+    private void FriendHazardReact(HazardType Haz)
+    {
+        switch (Haz)
+        {
+            case HazardType.Lava:
+                Debug.Log("friend lava");
+                FriendLava();
+                break;
+            case HazardType.Oil:
+                Debug.Log("friend oil");
+                break;
+            case HazardType.Electro:
+                Debug.Log("friend zap");
+                //FriendZap(FriendObj);
+                break;
+        }
     }
+
+    //I really don't want to mess with the singleton and save point controllers because I don't quite understand how they work
+    private void FriendLava() 
+    {
+        Debug.Log("Unclear how to integrate this with Jiachen's player respawn");
+    }
+
+    //oil is turned off for now
+    /*private IEnumerator FriendOilEffect()
+    {
+        //moveSpeed = +speedBoost;
+        //Debug.Log("friend speed " + moveSpeed);
+        yield return new WaitForSeconds(5f);
+        //moveSpeed = defSpeed;
+        //Debug.Log("friend speed " + moveSpeed);
+    }*/
+
+    /*private void FriendZap(GameObject Friend) 
+    {
+        frb = Friend.GetComponent<Rigidbody2D>();
+        frb.AddForce(frb.transform.right * -2f, ForceMode2D.Impulse);
+        Debug.Log("this happened " + frb.velocity);
+    }*/
+
 }
