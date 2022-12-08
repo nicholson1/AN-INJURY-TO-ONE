1085334c3b0edf09dfbc331199bfe80e4a46a634
Neil Nicholson <neil.nicholson.ny@gmail.com>
2022-12-07 21:39:15 -0500

    menu updates

--- AITO/Assets/IdleAd.cs
+++ AITO/Assets/IdleAd.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 public class IdleAd : MonoBehaviour
 {
+    public float moveSpeed = 10;
+    public float moveDist = 20;
+
+    private float initY;
+
+    private void Start()
+    {
+        initY = transform.localPosition.y;
+    }
+
     private void LateUpdate()
     {
-        this.transform.localPosition = new Vector3(0,Mathf.PingPong(Time.time * 10, 20));
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x,initY + Mathf.PingPong(Time.time * moveSpeed, moveDist), this.transform.localPosition.z);
     }
 }
