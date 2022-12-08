4e13f5ceeca5d515eac9f945c46f461fd2e7ca51
Jiachen Tang <tjc9606ok@gmail.com>
2022-12-07 21:27:38 -0500

    add two ButtonHazard and HazardDisableOnEvent to make buttons can turn off the attached hazard

---
+++ AITO/Assets/ButtonHazard.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHazard : MonoBehaviour
+{
+    [SerializeField] private Sprite ButtonUp;
+    [SerializeField] private Sprite ButtonDown;
+    [SerializeField] private SpriteRenderer image;
+
+    private HazardDisableOnEvent hazardDisable;
+    public bool StopsOnButtonUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hazardDisable = gameObject.GetComponentInChildren<HazardDisableOnEvent>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        PushHazardButton();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        LeaveHazardButton();
+    }
+
+    private void PushHazardButton()
+    {
+
+        if (hazardDisable != null)
+        {
+            hazardDisable.turnOff = true;
+        }
+
+        image.sprite = ButtonDown;
+        GetComponent<Interactable>().isInteractable = false;
+    }
+
+    private void LeaveHazardButton()
+    {
+        image.sprite = ButtonUp;
+        GetComponent<Interactable>().isInteractable = true;
+
+        //if (hazardDisable != null && StopsOnButtonUp)
+        //{
+        //    hazardDisable.turnOff = false;
+        //}
+    }
+}
---
+++ AITO/Assets/HazardDisableOnEvent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardDisableOnEvent : MonoBehaviour
+{
+    public bool turnOff = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (turnOff)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
