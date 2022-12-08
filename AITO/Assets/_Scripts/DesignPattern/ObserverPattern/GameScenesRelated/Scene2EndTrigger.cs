ba9cef8bfdf6cc274419a1b52eec350a3d11c6c6
Jiachen Tang <tjc9606ok@gmail.com>
2022-12-08 06:23:46 -0500

    Conveyor Belt scene prototype complete

---
+++ AITO/Assets/_Scripts/DesignPattern/ObserverPattern/GameScenesRelated/Scene2EndTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2EndTrigger : SceneEndTriggerController
+{
+    public override void ReturnLog()
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("Scene 2 End Observered");
+    }
+
+    public override int ReturnSceneIndex()
+    {
+        //throw new System.NotImplementedException();
+        return this.sceneIndex;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.sceneIndex = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            this.sceneEnds = true;
+            Notify(this.sceneIndex, this.sceneEnds);
+        }
+    }
+}
